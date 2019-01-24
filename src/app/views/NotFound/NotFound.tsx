f44f32cce47b05d040957e90a38490197fe1ae13
martinsflavio <flaviodosreismartins@gmail.com>
2019-01-23 22:50:30 -0800

    adding notFound Page

---
+++ src/app/views/NotFound/NotFound.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const NotFound: React.FunctionComponent<{}> = () => {
+  return (
+    <div>
+      <h1>Page Not Found!</h1>
+    </div>
+  );
+};
+
+export default NotFound;
